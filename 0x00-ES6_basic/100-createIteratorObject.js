f459ccd66aaffb18031bff799d5d2c7fd1e9abab
ohendarko <ohendarko@gmail.com>
2024-04-22 23:47:48 +0000

    Iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,29 @@
+export default function createIteratorObject(report) {
+  const { allEmployees } = report;
+  const departments = Object.keys(allEmployees);
+  let curDepartmentIdx = 0;
+  let currentEmployeeIdx = 0;
+
+  return {
+    next() {
+      if (curDepartmentIdx >= departments.length) {
+        return { done: true };
+      }
+
+      const currentDepartment = departments[curDepartmentIdx];
+      const employeesInCurrentDepartment = allEmployees[currentDepartment];
+
+      if (currentEmployeeIdx >= employeesInCurrentDepartment.length) {
+        curDepartmentIdx++;
+        currentEmployeeIdx = 0;
+        return this.next();
+      }
+
+      const currentEmployee = employeesInCurrentDepartment[currentEmployeeIdx++];
+      return { value: currentEmployee, done: false };
+    },
+    [Symbol.iterator]() {
+      return this;
+    },
+  };
+}
