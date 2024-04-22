6fcbbe603030c91a986f0a13f96ce26a6dac4b56
ohendarko <ohendarko@gmail.com>
2024-04-22 23:49:25 +0000

    Iterate through objects

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let result = '';
-  let iteratorResult = reportWithIterator.next();
-  while (!iteratorResult.done) {
-    result += `${iteratorResult.value} | `;
-    iteratorResult = reportWithIterator.next();
-  }
-  result = result.slice(0, -3);
-  return result;
+  return [...reportWithIterator].join(' | ');
 }
