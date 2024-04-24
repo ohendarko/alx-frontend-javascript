50bff483109bbdcd23b22a5d92babd48aba184ee
ohendarko <ohendarko@gmail.com>
2024-04-24 23:52:49 +0000

    Handle multiple promises 2

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstname, lastName, filename) {
+export default async function handleProfileSignup(firstname, lastName, filename) {
   const userPromise = signUpUser(firstname, lastName);
   const photoPromise = uploadPhoto(filename);
   return Promise.allSettled([userPromise, photoPromise])
     .then((results) => results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+    }))).catch(() => {
+      console.error('An unexpected error occurred');
+    });
 }
