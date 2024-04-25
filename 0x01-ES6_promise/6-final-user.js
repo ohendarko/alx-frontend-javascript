151e5a7283c0498fe1fb606fcd4f4aab1bc4dd99
ohendarko <ohendarko@gmail.com>
2024-04-25 00:34:34 +0000

    back

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,23 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const [signUpResult, uploadResult] = await Promise.all([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName).catch((error) => ({ status: 'rejected', value: error })),
-    ]);
-    return [
-      {
-        status: signUpResult.status,
-        value: signUpResult.value || signUpResult.error,
-      },
-      {
-        status: uploadResult.status,
-        value: uploadResult.value || uploadResult.error,
-      },
-    ];
-  } catch (error) {
-    console.error('An unexpected error occurred:', error);
-  }
-  return [];
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }))).catch(() => {
+      console.error('An unexpected error occurred');
+    });
 }
