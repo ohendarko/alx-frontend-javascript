71f04b629af07aa07c13aed4393f189773b19636
ohendarko <ohendarko@gmail.com>
2024-04-25 00:25:51 +0000

    Await / Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    return { photo, user };
+  } catch (error) {
+    console.error('An error occurred:', error);
+    return { photo: null, user: null };
+  }
+}
+
+export default asyncUploadUser;
