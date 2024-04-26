76e1f9d5cd2e93f914bf703cf6cb2244846c01ed
ohendarko <ohendarko@gmail.com>
2024-04-26 00:17:11 +0000

    evcar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,24 +1,12 @@
 import Car from './10-car';
 
-const cloneMethod = Symbol('clone');
-
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
-
-    // eslint-disable-next-line func-names
-    this[cloneMethod] = function () {
-      return new Car(this._brand, this._motor, this._color);
-    };
   }
 
-  get range() {
-    return this._range;
-  }
-
-  cloneCar() {
-    const clonedCar = super.cloneCar();
-    return clonedCar;
+  static get [Symbol.species]() {
+    return Car;
   }
 }
