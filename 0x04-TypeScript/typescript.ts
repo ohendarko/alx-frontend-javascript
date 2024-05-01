0b048c9cfef5066f187f9d66608c4b64b3ece97f
ohendarko <ohendarko@gmail.com>
2024-05-01 21:36:12 +0000

    1. Lets build a Teacher interface

--- 0x04-TypeScript/typescript.ts
+++ 0x04-TypeScript/typescript.ts
@@ -2,3 +2,18 @@ interface User {
   name: string;
   id: number;
 }
+
+class UserAccount {
+  name: string;
+  id: number;
+
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
+
+const user: User = new UserAccount("Murphy", 1);
+
+type MyBool = true | false;
+
