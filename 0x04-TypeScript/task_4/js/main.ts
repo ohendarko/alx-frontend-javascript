0e34b1886ffc779cf9953d7030da4175e7447087
ohendarko <ohendarko@gmail.com>
2024-05-01 23:57:14 +0000

    Update task_4/js/main.ts

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,28 @@
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+// Teacher object
+export const cTeacher: Subjects.Teacher = {
+  firstName: "John",
+  lastName: "Doe",
+  experienceTeachingC: 10
+};
+
+// For Cpp subject
+console.log("C++:");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// For Java subject
+console.log("Java:");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// For React subject
+console.log("React:");
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
