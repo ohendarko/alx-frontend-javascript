9c8b7c2f82d03381a28b82a3310fa2128e4f2db6
ohendarko <ohendarko@gmail.com>
2024-05-01 00:19:07 +0000

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
 export default function cleanSet(set, startString) {
   const appends = [];
+  if (startString === '') {
+    return '';
+  }
   set.forEach((element) => {
     if (element.startsWith(startString)) {
       appends.push(element.slice(startString.length));
