38f731fcdcc1d57506bf41fd5c8357af79c2260b
ohendarko <ohendarko@gmail.com>
2024-05-01 23:43:33 +0000

    empty

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++
@@ -1,8 +0,0 @@
-namespace Subjects {
-  export class Subject {
-    protected _teacher: Teacher;
-    public setTeacher = (teacher: Teacher) => {
-      this._teacher = teacher;
-    }
-  }
-}
--- 0x04-TypeScript/task_4/js/subjects/Teacher.ts
+++
@@ -1,9 +0,0 @@
-namespace Subjects {
-  export interface Teacher {
-    firstName: string;
-    lastName: string;
-    experienceTeachingC?: number;
-    experienceTeachingJava?: number;
-    experienceTeachingReact?: number;
-  }
-}
